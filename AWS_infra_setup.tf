66a6f7c79a875c95c7c476920760d4666ed6f6c4
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2023-02-11 09:55:31 +0530

    Update AWS_infra_setup.tf

--- AWS_infra_setup.tf
+++ AWS_infra_setup.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "edu-vpc" {
    cidr_block        = "10.0.1.0/24"
    map_public_ip_on_launch = true
    depends_on = [aws_vpc.edu-vpc]
-
+   availability_zone = "us-west-2a"
    tags = {
      Name = "edu-subnet"
    }
